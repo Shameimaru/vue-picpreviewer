bfbc4f1793d6c10d3bf010f1ea139b2264941c7c
Administrator <1>
2017-09-30 14:10:16 +0800

    adding preview for rich text

--- demo/App.vue
+++ demo/App.vue
@@ -7,9 +7,9 @@
         </div>
         <div>
             <image-previewer
-                class="image-plate"
-                :imageArr="imageArr1"
-                tag="img"
+                    class="image-plate"
+                    :images="images1"
+                    tag="img"
             >
             </image-previewer>
         </div>
@@ -20,9 +20,9 @@
         <div>
             <ul>
                 <image-previewer
-                    class="image-plate"
-                    :imageArr="imageArr2"
-                    tag="li"
+                        class="image-plate"
+                        :images="images2"
+                        tag="li"
                 >
                 </image-previewer>
             </ul>
@@ -34,41 +34,41 @@
         <div>
             <ul>
                 <image-previewer
-                    class="image-plate"
-                    :imageArr="imageArr3"
-                    tag="none"
-                    viewclass="image_view"
+                        class="image-plate"
+                        :images="images3"
+                        tag="none"
+                        viewclass="image_view"
                 >
                     <div class="article">
-                        <img class="images image_view" src="./assets/images/5.jpg" />
+                        <img class="images image_view" src="./assets/images/5.jpg"/>
                         <p class="article-header">右侧文字标题</p>
                         <p>
                             右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容
                         </p>
                     </div>
                     <div class="article">
-                        <img class="images image_view" src="./assets/images/3.jpg" />
+                        <img class="images image_view" src="./assets/images/3.jpg"/>
                         <p class="article-header">右侧文字标题</p>
                         <p>
                             右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容
                         </p>
                     </div>
                     <div class="article">
-                        <img class="images image_view" src="./assets/images/8.jpg" />
+                        <img class="images image_view" src="./assets/images/8.jpg"/>
                         <p class="article-header">右侧文字标题</p>
                         <p>
                             右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容
                         </p>
                     </div>
                     <div class="article">
-                        <img class="images image_view" src="./assets/images/2.jpg" />
+                        <img class="images image_view" src="./assets/images/2.jpg"/>
                         <p class="article-header">右侧文字标题</p>
                         <p>
                             右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容
                         </p>
                     </div>
                     <div class="article">
-                        <img class="images image_view" src="./assets/images/6.jpg" />
+                        <img class="images image_view" src="./assets/images/6.jpg"/>
                         <p class="article-header">右侧文字标题</p>
                         <p>
                             右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容右侧文字内容
@@ -77,15 +77,26 @@
                 </image-previewer>
             </ul>
         </div>
+        <div style="clear: both"></div>
+        <div>
+            <image-previewer
+                    class="image-plate23"
+                    tag="richtext"
+            >
+                <div v-html="richTextSegment">
+                </div>
+            </image-previewer>
+        </div>
     </div>
 </template>
 
 <script>
-    import { ImagePreviewer } from '../src/index.js';
+    import {ImagePreviewer} from '../src/index.js';
+
     export default {
         data() {
             return {
-                imageArr1: [{
+                images1: [{
                     src: require('./assets/images/1.jpg'),
                     name: 'a pic'
                 }, {
@@ -97,7 +108,7 @@
                 }, {
                     src: require('./assets/images/5.jpg')
                 }],
-                imageArr2: [{
+                images2: [{
                     src: require('./assets/images/6.jpg')
                 }, {
                     src: require('./assets/images/7.png')
@@ -108,7 +119,7 @@
                 }, {
                     src: require('./assets/images/10.jpg')
                 }],
-                imageArr3: [{
+                images3: [{
                     src: require('./assets/images/5.jpg')
                 }, {
                     src: require('./assets/images/3.jpg')
@@ -118,7 +129,21 @@
                     src: require('./assets/images/2.jpg')
                 }, {
                     src: require('./assets/images/6.jpg')
-                }]
+                }],
+                richTextSegment: '<img\n' +
+                '                            style="width: 120px; height: 120px;"\n' +
+                '                            class="images image_view"\n' +
+                '                            src="http://wxtestpb-10048617.image.myqcloud.com/3154e6ff-f68c-4a91-a66a-90316e6d505c?imageView2/q/100/w/800,"\n' +
+                '                    />\n' +
+                '                    <p>段落1</p>\n' +
+                '                    <div\n' +
+                '                            class="image_view"\n' +
+                '                            style=\n' +
+                '                                    "background: url(http://wxtestpb-10048617.image.myqcloud.com/194e0f4a-2681-4c94-9c8e-29d18dfb70b2?imageView2/q/100/w/800,) center center / cover no-repeat;\n' +
+                '                                        width: 100px;\n' +
+                '                                        height: 70px"\n' +
+                '\n' +
+                '                    ></div>'
             }
         },
         components: {
@@ -135,6 +160,7 @@
             padding: 10px 0;
         }
     }
+
     .article {
         clear: both;
         margin: 10px 0;
@@ -146,6 +172,7 @@
             font-weight: bold;
         }
     }
+
     .image-plate .images {
         width: 120px;
         height: 120px;
--- src/components/ImagePreviewer.vue
+++ src/components/ImagePreviewer.vue
@@ -1,26 +1,26 @@
 <template>
-    <div>
+    <div class="previewer">
         <div @click="imageClickHandler">
             <div
-                v-if="isShowPlate"
-                class="images"
-                :is="tag"
-                v-for="(imageObj, index) in imageArr"
-                :key="index"
-                :src="isImg ? imageObj.src : false"
-                :style="isImg ? {} : { backgroundImage: 'url(' + imageObj.src + ')' }"
+                    v-if="isShowPlate"
+                    class="images"
+                    :is="tag"
+                    v-for="(imageObj, index) in imageArr"
+                    :key="index"
+                    :src="isImg ? imageObj.src : false"
+                    :style="isImg ? {} : { backgroundImage: 'url(' + imageObj.src + ')' }"
             ></div>
-            <div class="slot-container">
+            <div class="slot-container" ref="slotContainer">
                 <slot></slot>
             </div>
             <previewer-dialog
-                ref="previewer"
-                :src="picSrc"
-                :size="picSize"
-                :name="picName"
-                @next-pic="nextPic"
-                @prev-pic="prevPic"
-                @play-pic="playPic"
+                    ref="previewer"
+                    :src="picSrc"
+                    :size="picSize"
+                    :name="picName"
+                    @next-pic="nextPic"
+                    @prev-pic="prevPic"
+                    @play-pic="playPic"
             ></previewer-dialog>
         </div>
 
@@ -35,15 +35,22 @@
         data() {
             return {
                 currentIndex: 0,
-                sizeArr: []
+                sizeArr: [],
+                imageArr: []
             }
         },
         computed: {
             isShowPlate() {
-                return this.tag !== 'none';
+                return this.tag !== 'none' && this.tag !== 'richtext';
             },
             picSrc() {
-                return this.imageArr[this.currentIndex].src;
+                if (this.imageArr.length !== 0) {
+                    return this.imageArr[this.currentIndex].src
+                }
+                else {
+                    return '';
+                }
+
             },
             picSize() {
                 if (this.sizeArr.length !== 0) {
@@ -56,7 +63,7 @@
                 }
             },
             picName() {
-                if (this.imageArr[this.currentIndex].name !== undefined) {
+                if (this.imageArr.length !== 0 && this.imageArr[this.currentIndex].name !== undefined) {
                     return this.imageArr[this.currentIndex].name;
                 }
                 return '';
@@ -67,32 +74,57 @@
         },
         methods: {
             imageClickHandler(event) {
-                const target = event.target;
-                let allNodes = null;
-                let isIndexChanged = false;
-
-                if (target.tagName.toLowerCase() === this.tag.toLowerCase()) {
-                    allNodes = target.parentNode.getElementsByClassName('images');
-                    isIndexChanged = true;
-                }
-                else if (target.classList.contains(this.viewclass)) {
-                    let closest = target.parentNode;
-                    while (!closest.classList.contains('slot-container')) {
-                        closest = closest.parentNode;
-                    }
-                    allNodes = closest.getElementsByClassName(this.viewclass);
-                    isIndexChanged = true;
-
-                }
-                if(isIndexChanged) {
-                    let index = -1;
-                    for (let i = 0; i < allNodes.length; i++) {
-                        if (allNodes[i] === target) {
-                            index = i;
+                let currentIndex = 0;
+                if (this.tag === 'richtext') {
+                    this.imageArr = [];
+                    const elCollection = this.$refs.slotContainer.querySelectorAll('img, .image_view');
+                    for(let i = 0, length = elCollection.length; i < length ; i++ ) {
+                        const el = elCollection[i];
+                        if(el === event.target) {
+                            currentIndex = i;
+                        }
+                        if(el.tagName.toUpperCase() === 'IMG') {
+                            this.imageArr.push({
+                                src: el.src
+                            });
+                        } else {
+                            const backgroundImage = el.style.backgroundImage;
+                            this.imageArr.push({
+                                src: /^url\("?(.+)"?\)$/g.exec(backgroundImage)[1]
+                            });
                         }
                     }
+                    console.log(this.imageArr);
+                    this.currentIndex = currentIndex;
                     this.$refs.previewer.show();
-                    this.currentIndex = index;
+                } else {
+                    const target = event.target;
+                    let allNodes = null;
+                    let isIndexChanged = false;
+
+                    if (target.tagName.toLowerCase() === this.tag.toLowerCase()) {
+                        allNodes = target.parentNode.getElementsByClassName('images');
+                        isIndexChanged = true;
+                    }
+                    else if (target.classList.contains(this.viewclass)) {
+                        let closest = target.parentNode;
+                        while (!closest.classList.contains('slot-container')) {
+                            closest = closest.parentNode;
+                        }
+                        allNodes = closest.getElementsByClassName(this.viewclass);
+                        isIndexChanged = true;
+
+                    }
+                    if (isIndexChanged) {
+                        let index = -1;
+                        for (let i = 0; i < allNodes.length; i++) {
+                            if (allNodes[i] === target) {
+                                index = i;
+                            }
+                        }
+                        this.$refs.previewer.show();
+                        this.currentIndex = index;
+                    }
                 }
             },
             nextPic() {
@@ -109,13 +141,18 @@
                 this.currentIndex = (this.currentIndex + 1 + this.imageArr.length) % this.imageArr.length;
             }
         },
+        created() {
+            if(this.images !== undefined) {
+                this.imageArr = this.images;
+            }
+        },
         components: {
             PreviewerDialog
         },
         props: {
-            imageArr: {
+            images: {
                 type: Array,
-                required: true
+                required: false
             },
             tag: {
                 type: String,
